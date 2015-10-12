b5b810752742eece0b284119034e74bddd90cf16
Marinela Sanchez <012137851@ecs414lin144.cecs.csulb.edu>
2015-10-12 13:03:30 -0700

    Implemting MapNOde.java

--- MapNode.java
+++ MapNode.java
@@ -1,5 +1,27 @@
-package CSULBLMAP;
+package CSULBMAP;
+
+import java.io.*;
+import java.util.Scanner;
 
 public class MapNode {
-	
-}
+	public String currentLocation;
+	public class Node extends MapNode{
+		public int[] possibleRooms;
+		public String fileName="mapNode.txt";
+		public int counter;
+		public void getRooms() throws FileNotFoundException{
+		
+			File file=new File(fileName);
+			Scanner input =new Scanner(file);
+			input.close();
+			//looks for room
+			if(input.hasNext(currentLocation)){
+				//count number of elements in array
+				
+			}
+		}
+	}
+					
+		
+		
+} 
